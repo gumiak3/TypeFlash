4725cc451a7cbbdba3ca5eca5d78687be21c92a9
Lukasz <gumiakk1234@gmail.com>
2023-09-19 00:20:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App(){
+    return (
+        <main className="min-h-screen text-white">
+            <header className="m-4 ">
+
+            </header>
+        </main>
+    )
+}
