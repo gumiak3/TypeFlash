7722c8be6d1d9b485f43e1658275050d3ecb2204
Lukasz Gumienniczuk <01178841@pw.edu.pl>
2023-10-24 22:58:14 +0200

    working on a key detection

--- src/components/typingCore/TypingContainer.tsx
+++ src/components/typingCore/TypingContainer.tsx
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {getData} from './fetchData';
 import {DictionaryController} from "./DictionaryController";
 import {Word} from './Word'
 
+
 export default function TypingContainer() {
     const [dictionary  , setDictionary ] = useState([""]);
-
+    const [position, setPosition] = useState(0);
+    const [keyPressed, setKeyPressed] = useState('');
     useEffect(() => {
         getData().then((data) => {
             const filteredData : string[] = DictionaryController.getInstance().filter(data);
@@ -15,14 +17,20 @@ export default function TypingContainer() {
             console.error(err);
         })
     }, []);
-    let id = 0;
+
+    function handleDataFromChild(data : number){
+        setPosition(data);
+    }
     return (
         <section>
             <ul className="flex flex-wrap gap-4 text-gray-500">
-                {dictionary.map((word) => (
+                {dictionary.map((word,index) => (
                     <Word
-                        key={id++}
+                        active={position === index}
+                        key={index}
+                        id={index}
                         content={word}
+                        sendDataToParent={handleDataFromChild}
                     />
                 ))}
             </ul>
--- src/components/typingCore/Word.tsx
+++ src/components/typingCore/Word.tsx
@@ -1,19 +1,58 @@
+import {useEffect, useState} from "react";
 
 interface wordProps{
+    active : boolean
+    id : number
     content : string
+    sendDataToParent : (data : number) => void;
 }
-export function Word({content} : wordProps){
-    function getLetters(word : string){
-        const letters = [];
-        for(let i=0;i<word.length;i++){
-            letters.push(word[i]);
+export function Word({active,id,content, sendDataToParent} : wordProps){
+    const [position, setPosition] = useState(0);
+    const [states, setStates] = useState(initPositionState(content))
+
+    function initPositionState(word: string){
+        return getLetters(word).map((letter, index) =>'');
+    }
+    function getLetters(word : string) : string[]{
+        return word.split('');
+    }
+    function handleSetStates(index : number,state : string){
+        const newArray = [...states];
+        newArray[index] = state;
+        setStates(newArray);
+    }
+    useEffect(()=>{
+        if(!active){
+            return;
         }
-        return letters;
+        function handleKeyPress(e : any){
+            const letter = getLetters(content)[position];
+            if(e.key === 'Backspace' && position >=0 ){
+                setPosition((prevPosition) => prevPosition - 1);
+            }else if(active && position < content.length && e.key !== 'Backspace'){
+                handleSetStates(position, letter === e.key ? 'correct' : 'incorrect');
+                setPosition((prevPosition) => prevPosition + 1);
+            }
+        }
+        window.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        }
+    },[content, position, states])
+    const letters = getLetters(content);
+    if(active){
+        return (
+            <li key={id} className={`select-none active`}>{letters.map((letter, index) => (
+                <span className={index <= position ? states[index] : '' } key={index}>{letter}</span>
+            ))}</li>
+        )
+    }else{
+        return (
+            <li key={id} className={`select-none`}>{getLetters(content).map((letter, index) => (
+                <span  key={index}>{letter}</span>
+            ))}</li>
+        )
     }
 
-    return (
-        <li>{getLetters(content).map(letter => (
-            <span>{letter}</span>
-        ))}</li>
-    )
 }
