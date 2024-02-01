d451cae064f1935cfb48171fd8e5a4ffc525bcbd
gumiak3 <gumiakk1234@gmail.com>
2024-02-01 15:14:56 +0100

    working on cursor animation

--- src/components/typingCore/Cursor.tsx
+++ src/components/typingCore/Cursor.tsx
@@ -11,13 +11,36 @@ interface cursorProps{
 export function Cursor({top, left}: cursorProps){
     const [position, setPosition] = useState(cursorStyle);
     const cursorRef = useRef<HTMLDivElement>(null);
+    const [typing, setTyping] = useState('not-typing');
     useEffect(()=>{
+        let timeoutId : NodeJS.Timeout;
         if(cursorRef.current){
             cursorRef.current.style.top = `${top}px`;
             cursorRef.current.style.left = `${left}px`;
         }
-    },[top,left]);
+        function handleKeyDown(){
+            if(typing !== 'typing'){
+                setTyping('typing');
+                if(timeoutId){
+                    clearTimeout(timeoutId)
+                }
+            }
+
+        }
+        function handleKeyUp(){
+            timeoutId = setTimeout(()=>{
+                setTyping('not-typing');
+            }, 1000);
+        }
+        window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+        return ()=>{
+            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
+            clearTimeout(timeoutId);
+        }
+    },[top,left, typing]);
     return (
-        <div ref={cursorRef} style={cursorStyle} className={"letter-cursor-typing bg-white w-px h-7 absolute"}></div>
+        <div ref={cursorRef} style={cursorStyle} className={`letter-cursor-${typing} bg-white w-px h-7 absolute`}></div>
     )
 }
